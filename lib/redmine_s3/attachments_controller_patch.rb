fbe55dc68e2e3969a04970048b599d6d45167aa2
Kim Pepper <kim@pepper.id.au>
2014-12-12 13:46:07 +1100

    Revert "enabled inline view with pach like files"
    This reverts commit c652edf5dcc49d21c1ed8b9720ea4da83132e4fa.

--- lib/redmine_s3/attachments_controller_patch.rb
+++ lib/redmine_s3/attachments_controller_patch.rb
@@ -22,16 +22,10 @@ module RedmineS3
           @attachment.increment_download
         end
         if RedmineS3::Connection.proxy?
-          disposition = if @attachment.image? || @attachment.text?
-                          'inline'
-                        else
-                          'attachment'
-                        end
-
           send_data RedmineS3::Connection.get(@attachment.disk_filename),
                                           :filename => filename_for_content_disposition(@attachment.filename),
                                           :type => detect_content_type(@attachment),
-                                          :disposition => disposition
+                                          :disposition => (@attachment.image? ? 'inline' : 'attachment')
         else
           redirect_to(RedmineS3::Connection.object_url(@attachment.disk_filename))
         end
